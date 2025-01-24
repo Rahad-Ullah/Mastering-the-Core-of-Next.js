77b8b79a702e85b2ada18575b51f5da7b76b6c04
Md. Rahad Ullah <rahadullah10@gmail.com>
2025-01-24 11:56:12 +0600

    refactor: update post fetching logic to support ISR and SSR; add loading state for posts

--- src/app/(withCommonLayout)/posts/page.tsx
+++ src/app/(withCommonLayout)/posts/page.tsx
@@ -1,8 +1,24 @@
+import PostCard from "@/components/ui/PostCard";
+import { getAllPosts } from "@/services/PostServices";
+import { TPost } from "@/types";
 
 const AllPostsPage = async () => {
+  const posts = await getAllPosts("ssr", true);
+
   return (
-    <div>
-      <h1>All Posts</h1>
+    <div className="my-10 w-[90%] mx-auto">
+      <h1 className="text-4xl text-center">
+        All <span className="text-fuchsia-800">Posts</span>
+      </h1>
+      <p className="text-gray-400 text-center italic w-2/4 mx-auto mt-2">
+        Discover, review, and rate your favorite books. Join a community of book
+        lovers and share your reading experiences.
+      </p>
+      <div className="grid grid-cols-3 my-12 gap-8">
+        {posts.map((post: TPost) => (
+          <PostCard key={post.id} post={post} />
+        ))}
+      </div>
     </div>
   );
 };
