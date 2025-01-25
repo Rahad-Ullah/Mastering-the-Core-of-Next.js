3899f8466c675f8dc6fe47b8c23e3a5476699bf0
Md. Rahad Ullah <rahadullah10@gmail.com>
2025-01-25 15:52:58 +0600

    feat: add NotFound page for handling 404 errors

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+const NotFoundPage = () => {
+  return (
+    <div className="flex flex-col justify-center items-center gap-2 min-h-screen">
+      <h1 className="text-5xl font-extrabold">404</h1>
+      <h1 className="text-xl font-medium">Page Not Found</h1>
+    </div>
+  );
+};
+
+export default NotFoundPage;
