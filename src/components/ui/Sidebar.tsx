ea7052bebcb532f4e0963e89853a99ccb6054d6a
Md. Rahad Ullah <rahadullah10@gmail.com>
2025-01-25 15:46:27 +0600

    feat: add Dashboard page and layout; implement Sidebar with navigation links

--- src/components/ui/Sidebar.tsx
+++ src/components/ui/Sidebar.tsx
@@ -15,8 +15,11 @@ const Sidebar = ({ children }: { children: ReactNode }) => {
           aria-label="close sidebar"
           className="drawer-overlay"
         ></label>
-        <ul className="menu bg-fuchsia-100 text-base-content min-h-full w-80 p-4">
+        <ul className="menu bg-fuchsia-100 text-base-content min-h-full w-80 p-4 space-y-2">
           {/* Sidebar content here */}
+          <li className="bg-fuchsia-200 rounded">
+            <Link href={"/"}>🏠 Home</Link>
+          </li>
           <li className="bg-fuchsia-200 rounded">
             <Link href={"/dashboard/create-post"}>📝Create a Post</Link>
           </li>
